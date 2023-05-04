e8d3e18ab34a8112d91a9e6cff6134cce384a049
MnCSSJ4x <Monjoy.Choudhury@iiitb.ac.in>
2023-05-05 00:27:15 +0530

    Final

--- frontend/src/pages/select.tsx
+++
@@ -1,9 +0,0 @@
-import Main from "./select/components/main"
-
-const Select = () => {
-  return (
-    <div><Main></Main></div>
-  )
-}
-
-export default Select
--- frontend/src/pages/welcome/components/main.tsx
+++ frontend/src/pages/welcome/components/main.tsx
@@ -17,9 +17,8 @@ const Main = (props:any) => {
    
     formData.append('sentBy',uid?.toString())
     formData.append('file', file!);
-    axios.post(BACKEND_HOME+'/upload', formData).then((response)=>{
-      console.log("TIS DONE");
-      router.push({pathname: '/select', query: { userID: response.data.userID }}); 
+    axios.post(BACKEND_HOME+'/upload', formData).then(()=>{
+      console.log("TIS DONE")
     }).catch(error => {
       console.log(error)
     })
